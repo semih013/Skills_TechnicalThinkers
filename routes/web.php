6f706b0aeca4a801cc04cc09edc16d0f2dfc508a
woutverheyen <r138339@student.thomasmore.be>
2026-03-17 13:04:42 +0100

    l

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,7 @@ Route::middleware(['auth', 'verified'])->group(function () {
 
     Route::get('/farmers', [FarmerController::class, 'index'])->name('farmers.index');
 
+    Route::get('/alerts', [AlertController::class, 'index'])->name('alerts.index');
     Route::get('/alerts/create', [AlertController::class, 'create'])->name('alerts.create');
     Route::post('/alerts/preview', [AlertController::class, 'preview'])->name('alerts.preview');
     Route::post('/alerts/send', [AlertController::class, 'send'])->name('alerts.send');
